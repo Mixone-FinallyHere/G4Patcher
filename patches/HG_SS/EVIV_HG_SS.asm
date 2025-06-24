df529f926391ac756b74552f159ad0749fb953f4
Miguel <miguel.terol.93@gmail.com>
2025-06-24 23:04:59 +0200

    Fix immediate address?

--- patches/HG_SS/EVIV_HG_SS.asm
+++ patches/HG_SS/EVIV_HG_SS.asm
@@ -42,7 +42,10 @@ EV_IV_Viewer:
     beq  .write_stats_ids
 
 .write_ev_ids:
-    ldr r0, [r7, #0x7AC]    ; Pointer in sumamry struct to string variable for some reason
+    mov r1, #0x7A
+    lsl r1, r1, #0x4
+    add r1, #0xC
+    ldr r0, [r7, r1]    ; Pointer in sumamry struct to string variable for some reason
     bl 0x020263AC           ; String_SetEmpty
     ldr r1, =evString      ; "EV" + EOS
     bl 0x020269A0           ; CopyU16ArrayToString
