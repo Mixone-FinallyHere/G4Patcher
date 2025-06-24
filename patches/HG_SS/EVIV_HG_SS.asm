d1bcf40d6f8faf094458ce6748ffdad3f31f0606
Miguel <miguel.terol.93@gmail.com>
2025-06-24 22:57:17 +0200

    EV string change

--- patches/HG_SS/EVIV_HG_SS.asm
+++ patches/HG_SS/EVIV_HG_SS.asm
@@ -22,22 +22,35 @@ INJECT_ADDR equ 0x023C8000
 
 EV_IV_Viewer:
     push {lr}
-    bl 0x0206FD00   ; original function
+    bl 0x0206FD00           ; original function
     push {r0}
 
-    ldr  r3, =0x021D110C     ; gSystem
-    ldr  r2, [r3, #0x44]     ; heldKeys
-    ldr  r3, =0x0100         ; R button
+    sub sp, #0x4            ; allocate space for X on stack
+    mov r0, #0x0            ; put 0 into r0 to send to stack
+    str r0, [sp, #0x0]      ; Put it at start
+
+    ldr  r3, =0x021D110C    ; gSystem
+    ldr  r2, [r3, #0x44]    ; heldKeys
+    ldr  r3, =0x0100        ; R button
     tst  r2, r3
     bne  .write_iv_ids
 
     ldr  r3, =0x021D110C
     ldr  r2, [r3, #0x44]
-    ldr  r3, =0x0200         ; L button
+    ldr  r3, =0x0200        ; L button
     tst  r2, r3
     beq  .write_stats_ids
 
 .write_ev_ids:
+    ldr r0, [r7, #0x7AC]    ; Pointer in sumamry struct to string variable for some reason
+    bl 0x020263AC           ; String_SetEmpty
+    ldr r1, =evString      ; "EV" + EOS
+    bl 0x020269A0           ; CopyU16ArrayToString
+    mov r0, r7              ; Summary Struct param 1
+    mov r1, #0x2            ; Window ID 2 is param 2
+    ldr r2, =0xe0f00        ; Color white is param 3
+    bl 0x0208C850           ; sub_0208C850(int sumamryStruct,int windowID,s32 msgBank,u32 colour,u32 x)
+
     ldr r0, =0x020899CC
     mov r1, #13
     strb r1, [r0]
@@ -109,8 +122,13 @@ EV_IV_Viewer:
     strb r1, [r0]
 
 .return:
+    add sp, #0x4
     pop {r0,pc}
 
+evString:
+    .byte 0x00, 0xB0, 0x00, 0xC1, 0xFF, 0xFF    ; "EV" + EOS
+
 .pool
+
 
 .close
